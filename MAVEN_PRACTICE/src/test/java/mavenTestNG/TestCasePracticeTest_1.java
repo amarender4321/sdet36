2ea4ecd6ec84c4a2966a90af67169e8545575b8d
AMARENDER <AMARENDER@DESKTOP-OG1P3UF>
2022-07-19 12:22:57 +0530

    commit

---
+++ MAVEN_PRACTICE/src/test/java/mavenTestNG/TestCasePracticeTest_1.java
@@ -0,0 +1,19 @@
+package mavenTestNG;
+
+import org.testng.annotations.Test;
+
+public class TestCasePracticeTest_1 {
+	@Test(priority = 3)
+	public void test1() {
+		System.out.println("Test Case 1");
+	}
+	@Test(priority = 1)
+	public void test2() {
+		System.out.println("Test Case 2");
+	}
+	@Test(priority = 2)
+	public void test3() {
+		System.out.println("Test Case 3");
+	}
+
+}
---
+++ MAVEN_PRACTICE/src/test/java/mavenTestNG/TestCasePracticeTest_2.java
@@ -0,0 +1,23 @@
+package mavenTestNG;
+
+import static org.testng.Assert.fail;
+
+import org.testng.annotations.Test;
+
+public class TestCasePracticeTest_2 {
+	
+	@Test(dependsOnMethods = "mavenTest5" )
+	public void mavenTest4() {
+		System.out.println("maven Test 4");
+	}
+	@Test
+	public void mavenTest5() {
+		System.out.println("maven Test 5");
+		
+	}
+	@Test
+	public void mavenTest6() {
+		System.out.println("maven Test 6");
+	}
+
+}
